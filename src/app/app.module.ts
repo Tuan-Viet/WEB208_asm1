0106ea83037b017f1cf22b050cb4189aaa0348bd
TuanViet <vietlt226688@gmail.com>
2023-05-28 10:16:39 +0700

    Add product

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './pages/product-list/product-list.component';
@@ -21,7 +22,10 @@ import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.compon
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule, HttpClientModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/product-add/product-add.component.ts
+++ src/app/pages/product-add/product-add.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder } from '@angular/forms';
+import { IProduct } from 'src/app/interfaces/Product';
+import { ProductService } from 'src/app/services/product.service';
+
 
 @Component({
   selector: 'app-product-add',
@@ -6,5 +11,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-add.component.css']
 })
 export class ProductAddComponent {
+  constructor(private productService: ProductService, private formBuilder: FormBuilder, private router: Router) {
 
+  }
+  productForm = this.formBuilder.group({
+    name: [''],
+    price: [0]
+  })
+
+  onHandleSubmit() {
+    const product: IProduct = {
+      id: '',
+      name: this.productForm.value.name || '',
+      price: this.productForm.value.price || 0
+    }
+
+    this.productService.addProduct(product).subscribe(data => {
+      console.log(data);
+      alert("Them thanh cong")
+      this.router.navigate(['product'])
+    })
+  }
 }
