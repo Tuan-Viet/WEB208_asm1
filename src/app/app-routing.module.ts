afc116a05898d584d3728d390dde1e8875bff1fb
TuanViet <vietlt226688@gmail.com>
2023-05-28 10:28:38 +0700

    Detail product

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { ProductListComponent } from './pages/product-list/product-list.componen
 import { ProductAddComponent } from './pages/product-add/product-add.component';
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 import { ProductEditComponent } from './pages/product-edit/product-edit.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
 
 const routes: Routes = [
   {
@@ -11,7 +12,7 @@ const routes: Routes = [
       { path: '', redirectTo: "product", pathMatch: "full" },
       { path: 'product', component: ProductListComponent },
       { path: 'product/add', component: ProductAddComponent },
-      { path: 'product/:id', component: ProductAddComponent },
+      { path: 'product/:id', component: ProductDetailComponent },
       { path: 'product/:id/edit', component: ProductEditComponent },
     ]
   }
--- src/app/pages/product-detail/product-detail.component.ts
+++ src/app/pages/product-detail/product-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IProduct } from 'src/app/interfaces/Product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-detail',
@@ -6,5 +9,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent {
+  product: IProduct = {
+    id: '',
+    name: '',
+    price: 0
+  }
+  constructor(private route: ActivatedRoute, private productService: ProductService) {
+    this.route.paramMap.subscribe(param => {
+      const id = Number(param.get('id'));
+      this.productService.getProductById(id).subscribe(product => {
+        this.product = product;
+      })
+    })
 
+  }
 }
